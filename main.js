037ba90c2af997c0d740df80287aa53c7834dc07
lucieauge <lucie.auge@gmail.com>
2018-12-21 02:36:12 +0100

    Ajoutpopup

---
+++ main.js
@@ -0,0 +1,9 @@
+	var message = "Hey le site xxxvidsxxx est trop bien. Viens dessus stp please. Ok ?"
+
+setTimeout(box, 10000);
+
+function box() {
+    if (confirm(message) == true) {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+}
